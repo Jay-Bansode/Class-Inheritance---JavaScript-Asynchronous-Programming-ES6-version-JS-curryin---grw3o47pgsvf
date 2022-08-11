a094ff1c3e689c079d3dc3bf6b5c7557f0dfaad3
Jay-Bansode <106896605+Jay-Bansode@users.noreply.github.com>
2022-08-11 18:40:35 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,13 @@
 
 
 // export class using module.exports
+var Shape = require('./shape.js');
+class Circle extends Shape{
+    constructor(color) {
+        super();
+    }
+    calculateArea() {
+
+    }
+}
+module.exports=Circle;
